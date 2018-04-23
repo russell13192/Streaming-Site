614b4d25f22d4f070a2e7a85e398d41f300a7475
George Murray <murrayge@mnstate.edu>
2018-04-23 00:22:52 -0500

    Begin Implementation of Mongo Authentication

--- server/server.js
+++ server/server.js
@@ -1,12 +1,61 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const cookieParser = require('cookie-parser');
 const hbs = require('hbs');
-
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const mongo = require('mongodb');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/murvieLogin');
+const db = mongoose.connection;
 var app = express();
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
-app.get('/', (req, res) => {
-   res.render('login.hbs'); 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.'),
+        root = namespace.shift(),
+        formParam = root;
+
+        while (namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param: formParam,
+            msg: msg,
+            value: value
+        };
+    }
+}));
+app.use(flash());
+
+app.use(function(req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
 });
+// app.get('/', (req, res) => {
+//    res.render('login.hbs'); 
+// });
 
 app.listen(3000);
