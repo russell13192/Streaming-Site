29d0a0a170fdc8dd38d931cd8b92600d16842e6a
George Murray <murrayge@mnstate.edu>
2018-04-28 19:48:19 -0500

    Login registration system completed with authentication protection

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var User = require('../models/user');
 
@@ -27,7 +29,7 @@ router.post('/register', function (req, res) {
     req.checkBody('username', 'Username is required').notEmpty();
     req.checkBody('password', 'Password is required').notEmpty();
     req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
-    
+
 
     var errors = req.validationErrors();
 
@@ -43,7 +45,7 @@ router.post('/register', function (req, res) {
             password: password
 
         });
-        User.createUser(newUser, function(err, user) {
+        User.createUser(newUser, function (err, user) {
             if (err) {
                 throw err;
             } else {
@@ -55,6 +57,52 @@ router.post('/register', function (req, res) {
 
         res.redirect('/users/login');
     }
+});
+
+passport.use(new LocalStrategy(
+    function (username, password, done) {
+       User.getUserByUsername(username, function(err, user) {
+           if (err) {
+               throw err;
+           }
+           if (!user) {
+               return done(null, false, {message: 'Username not found'});
+           }
+           User.comparePassword(password, user.password, function(err, isMatch) {
+                if (err) {
+                    throw err;
+                }
+                if (isMatch) {
+                    return done(null, user);
+                } else {
+                    return done(null, false, {message: 'Invalid Password'});
+                }
+           });
+       })
+    }
+));
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    User.getUserById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
+router.post('/login',
+    passport.authenticate('local', { successRedirect: '/', failureRedirect: '/users/login', failureFlash: true }),
+    function (req, res) {
+        res.redirect('/');
+    });
+router.get('/logout', function(req, res) {
+    req.logout();
+
+    req.flash('success_msg', 'You have successfully logged out');
+
+    res.redirect('/users/login');
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -57,8 +57,11 @@ app.use(function(req, res, next) {
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
     res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
     next();
 });
+
+
 // app.get('/', (req, res) => {
 //    res.render('login.hbs'); 
 // });
